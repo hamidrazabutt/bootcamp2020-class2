87a0a8ffa0810d704bb148fa2e1f76910d7a5573
hamidrazabutt <68273560+hamidrazabutt@users.noreply.github.com>
2020-11-08 03:59:33 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div>
-        Hello World From Hamid Raza.
+        <h1>Hello World From Hamid Raza.</h1>
     </div>
   );
 }
